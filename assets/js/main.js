e16260ca8a2e849213feb3e4eb8fa7d5384a0a84
Shayan Javid <shayantghzdh@gmail.com>
2024-06-27 09:31:24 +0200

    Control Panel
    Web-based interface designed to manage servers, proxies, and account settings efficiently. This project is built using HTML, CSS, and JavaScript, and includes both dark and light theme options for better user experience.

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+//Toggle mobile menu
+const menuToggleOpen = document.querySelector('.menu-toggle-open');
+const menuToggleClose = document.querySelector('.menu-toggle-close');
+const navMobileElement = document.querySelector('.nav-mobile');
+
+menuToggleOpen.addEventListener('click', () => {
+    navMobileElement.classList.add('active');
+});
+
+menuToggleClose.addEventListener('click', () => {
+    navMobileElement.classList.remove('active');
+});
+
+//Toggle theme
+const bodyElement = document.body;
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+if (currentTheme) {
+    bodyElement.classList.add('theme-light');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('theme-light');
+
+    if (bodyElement.classList.contains('theme-light')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
+});
+//Show dropdown menu
+document.addEventListener('click', (e) => {
+    const dropdownMenu = document.querySelector('.dropdown');
+
+    if(e.target.classList.contains('dropdown-btn')){
+        dropdownMenu.classList.add('active');
+    }else{
+        dropdownMenu.classList.remove('active');
+    }
+});
